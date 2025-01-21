7f4d855b831e4fdbbd795a9b78821f841c9e7597
Hyejun An <jagaldol.dev@gmail.com>
2025-01-21 22:59:18 +0900

    feat: tailwind font 및 배경 등 기본 세팅

--- chrome-extension/tailwind.config.js
+++ chrome-extension/tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "#eeeeee",
+        "main-theme": "#4064F6",
+        "text-gray": "#8D8E92",
+      },
+    },
+    fontFamily: {
+      GmarketSansMedium: ["GmarketSansMedium", "Arial", "Helvetica", "sans-serif"],
+      SUITRegular: ["SUIT-Regular", "Arial", "Helvetica", "sans-serif"],
+    },
   },
   plugins: [],
-}``
+}
