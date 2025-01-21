b1ec0f34832f34b8bd5ee344421a87948e75c65b
Hyejun An <jagaldol.dev@gmail.com>
2025-01-21 15:55:40 +0900

    fix: 불필요한 api 삭제 및 코드 정리

--- server/routers/auth_router.py
+++ server/routers/auth_router.py
@@ -1,18 +1,7 @@
-import os
-
-from dotenv import load_dotenv
 from fastapi import APIRouter, HTTPException, Request
 from pydantic import BaseModel
 
 from server.service import auth_service
-
-# Load environment variables
-load_dotenv()
-
-# Google OAuth2 setup
-CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
-CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET")
-REDIRECT_URI = "http://localhost:3000"
 
 # Initialize router
 auth_router = APIRouter(prefix="/auth", tags=["Authentication"])
@@ -35,22 +24,17 @@ async def logout(request: Request):
     return {"message": "Logged out"}
 
 
-class ValidateTokenRequest(BaseModel):
-    access_token: str
-
-
-@auth_router.post("/google/validate-token")
-async def validate_token(request_body: ValidateTokenRequest):
-    return auth_service.get_token_info(request_body.access_token)
-
-
-class RefreshTokenRequest(BaseModel):
-    refresh_token: str
-
-
 @auth_router.get("/google/profile")
 async def google_profile(request: Request):
     user_id = request.session.get("user_id")
     if not user_id:  # 세션에 user_id가 없는 경우
         raise HTTPException(status_code=401, detail="Authentication required. Please log in.")  # Unauthorized
     return await auth_service.get_google_profile(user_id)
+
+
+@auth_router.get("/google/callback")
+async def google_callback(request: Request):
+    code = request.query_params.get("code")
+    if not code:
+        raise HTTPException(status_code=400, detail="Code parameter is required.")
+    return code
