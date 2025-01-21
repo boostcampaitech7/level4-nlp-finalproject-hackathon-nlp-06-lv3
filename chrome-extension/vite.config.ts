83e4d5f97c1e2f66779aed60c6f48ca7814e9fc7
jagaldol <yoy06056@pusan.ac.kr>
2025-01-21 15:55:41 +0900

    chore: chrome extension 개발시 hot reload 가능

--- chrome-extension/vite.config.ts
+++ chrome-extension/vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import * as path from "path"
+import hotReloadExtension from "hot-reload-extension-vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    hotReloadExtension({
+      log: true,
+      backgroundPath: "src/app/App.tsx", // src/pages/background/index.ts
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
