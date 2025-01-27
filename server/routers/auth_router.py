88ff1fb34fd8e3f1fcca192b3f657e6d6e01c7df
Hyejun An <jagaldol.dev@gmail.com>
2025-01-27 18:33:53 +0900

    feat: Api Response을 통한 포맷 통일

--- server/routers/auth_router.py
+++ server/routers/auth_router.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Depends, Request
 
+from server._core.utils.api_response import ApiResponse
 from server.dependencies.session import get_user_id_from_session
 from server.schemas import auth_request, auth_response
 from server.service import auth_service
@@ -9,29 +10,33 @@ auth_router = APIRouter(prefix="/auth", tags=["Authentication"])
 
 
 @auth_router.get("/is-login")
-async def is_login(request: Request) -> auth_response.IsLoginDto:
+async def is_login(request: Request) -> ApiResponse[auth_response.IsLoginDto]:
     user_id = request.session.get("user_id")
-    return auth_service.is_login(user_id)
+    return ApiResponse.success(auth_service.is_login(user_id))
 
 
 @auth_router.post("/logout")
-async def logout(request: Request) -> auth_response.LogoutDto:
+async def logout(request: Request) -> ApiResponse[auth_response.LogoutDto]:
     request.session.clear()
-    return auth_service.logout()
+    return ApiResponse.success(auth_service.logout())
 
 
 @auth_router.post("/google")
-async def google_auth(request_dto: auth_request.GoogleAuthDto, request: Request) -> auth_response.GoogleAuthDto:
+async def google_auth(
+    request_dto: auth_request.GoogleAuthDto, request: Request
+) -> ApiResponse[auth_response.GoogleAuthDto]:
     response_dto, user_id = await auth_service.google_authenticatie(request_dto)
     request.session["user_id"] = user_id
-    return response_dto
+    return ApiResponse.success(response_dto)
 
 
 @auth_router.get("/google/profile")
-async def google_profile(user_id: int = Depends(get_user_id_from_session)) -> auth_response.GoogleProfileDto:
-    return await auth_service.get_google_profile(user_id)
+async def google_profile(
+    user_id: int = Depends(get_user_id_from_session),
+) -> ApiResponse[auth_response.GoogleProfileDto]:
+    return ApiResponse.success(await auth_service.get_google_profile(user_id))
 
 
 @auth_router.get("/google/callback")
-async def google_callback(code: str) -> auth_response.GoogleCallbackDto:
-    return auth_service.google_callback(code)
+async def google_callback(code: str) -> ApiResponse[auth_response.GoogleCallbackDto]:
+    return ApiResponse.success(auth_service.google_callback(code))
--- server/routers/report_router.py
+++ server/routers/report_router.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Depends
 
+from server._core.utils.api_response import ApiResponse
 from server.dependencies.session import get_user_id_from_session
 from server.schemas import report_response
 from server.service import report_service
@@ -11,5 +12,5 @@ report_router = APIRouter(prefix="/reports", tags=["Reports"])
 @report_router.get("/temp")
 async def get_reports_temp(
     user_id: int = Depends(get_user_id_from_session), page: int = 1, limit: int = 20
-) -> report_response.TempReportsDto:
-    return await report_service.get_reports(user_id, page, limit)
+) -> ApiResponse[report_response.TempReportsDto]:
+    return ApiResponse.success(await report_service.get_reports(user_id, page, limit))
