d5a9a573c74a5814aa07e6f01584b4f3d09b901f
Hyejun An <jagaldol.dev@gmail.com>
2025-01-27 16:07:36 +0900

    fix: 추후 사용하지 않을 임시 table temp를 사용함에 따라 엔드포인트 /temp 추가

--- server/routers/report_router.py
+++ server/routers/report_router.py
@@ -7,6 +7,6 @@ from server.service import report_service
 report_router = APIRouter(prefix="/reports", tags=["Reports"])
 
 
-@report_router.get("/")
+@report_router.get("/temp")
 async def get_reports(user_id: int = Depends(get_user_id_from_session), page: int = 1, limit: int = 20):
     return await report_service.get_reports(user_id, page, limit)
