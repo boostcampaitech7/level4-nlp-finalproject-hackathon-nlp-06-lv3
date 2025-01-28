fd64c5c85245058feb119725f5db1448cb8fa7b7
Gamchan Kang <104420634+gsgh3016@users.noreply.github.com>
2025-01-29 00:41:55 +0900

    feat: 프롬프트를 위한 옵션 추가

--- agents/utils.py
+++ agents/utils.py
@@ -4,19 +4,29 @@ from prompt import load_template, load_template_with_variables
 
 
 # YAML 파일에서 카테고리 정보 로드
-def load_categories_from_yaml(file_path: str) -> list:
+def load_categories_from_yaml(file_path: str, is_prompt: bool = False) -> list:
     """
     YAML 파일에서 카테고리 정보를 로드합니다.
 
     Args:
         file_path (str): YAML 파일 경로
-
+        is_prompt (bool): True일 경우 name과 rubric만 로드하고,
+                          False일 경우 name과 description만 로드합니다.
     Returns:
         list: 카테고리 정보 리스트
+
+    Raises:
+        FileNotFoundError: 파일이 존재하지 않을 경우 예외를 발생시킵니다.
+        ValueError: YAML 파일 파싱 중 오류가 발생할 경우 예외를 발생시킵니다.
     """
     try:
         with open(file_path, "r", encoding="utf-8") as file:
-            return yaml.safe_load(file)
+            categories: dict = yaml.safe_load(file)
+
+            if is_prompt:
+                return [{"name": category["name"], "rubric": category["rubric"]} for category in categories]
+            else:
+                return [{"name": category["name"], "description": category["description"]} for category in categories]
     except FileNotFoundError:
         raise FileNotFoundError(f"카테고리 파일 {file_path}이(가) 존재하지 않습니다.")
     except yaml.YAMLError as e:
@@ -24,7 +34,7 @@ def load_categories_from_yaml(file_path: str) -> list:
 
 
 # 카테고리 정보 로드
-CATEGORIES = load_categories_from_yaml("agents/classification/categories.yaml")
+CATEGORIES_FOR_OUTPUT = load_categories_from_yaml("agents/classification/categories.yaml")
 
 # 공통 JSON Schema 속성
 MAIL_PROPERTIES = {
@@ -88,7 +98,7 @@ REPORT_FORMAT = {
     "json_schema": {
         "name": "email_report",
         "strict": True,
-        "schema": create_category_schema(CATEGORIES),
+        "schema": create_category_schema(CATEGORIES_FOR_OUTPUT),
     },
 }
 
@@ -137,7 +147,7 @@ REFINE_FORMAT = {
     "json_schema": {
         "name": "revised_summarization",
         "strict": True,
-        "schema": create_category_schema(CATEGORIES),
+        "schema": create_category_schema(CATEGORIES_FOR_OUTPUT),
     },
 }
 
@@ -187,7 +197,7 @@ def generate_plain_text_report(formatted_report: dict | str) -> str:
         return formatted_report
 
     plain_text = ""
-    for category in CATEGORIES:
+    for category in CATEGORIES_FOR_OUTPUT:
         label = category["name"]
         description = category["description"]
         plain_text += f"\n{description}\n"
