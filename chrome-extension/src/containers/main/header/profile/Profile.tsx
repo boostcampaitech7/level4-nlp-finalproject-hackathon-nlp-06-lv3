a2bea5f10bf4f23ae43830a63b664e4eddbcee39
Hyejun An <jagaldol.dev@gmail.com>
2025-02-01 03:41:28 +0900

    feat: 프로필 열려있을 때 바깥 클릭 시 닫힘

--- chrome-extension/src/containers/main/header/profile/Profile.tsx
+++ chrome-extension/src/containers/main/header/profile/Profile.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import useUserInfoQuery from "@/hooks/useUserInfoQuery"
 import ProfileInfoBox from "@/containers/main/header/profile/ProfileInfoBox"
 
@@ -6,12 +6,36 @@ export default function Profile() {
   const { userInfo } = useUserInfoQuery()
   const [isOpenInfoBox, setIsOpenInfoBox] = useState(false)
 
+  const openedRef = useRef<HTMLDivElement>(null)
+  const buttonRef = useRef<HTMLButtonElement>(null)
+
+  const handleClickOutside = (event: MouseEvent) => {
+    if (openedRef.current && !openedRef.current.contains(event.target as Node)) {
+      if (buttonRef.current && !buttonRef.current.contains(event.target as Node)) setIsOpenInfoBox(false)
+    }
+  }
+
+  useEffect(() => {
+    if (isOpenInfoBox) {
+      document.addEventListener("mousedown", handleClickOutside)
+    } else {
+      document.removeEventListener("mousedown", handleClickOutside)
+    }
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside)
+    }
+  }, [isOpenInfoBox])
+
   return (
     <div className="relative">
-      <button type="button" onClick={() => setIsOpenInfoBox(!isOpenInfoBox)}>
+      <button type="button" ref={buttonRef} onClick={() => setIsOpenInfoBox(!isOpenInfoBox)}>
         <img src={userInfo.picture} alt="profile" className="rounded-full border border-border-gray w-10 h-10" />
       </button>
-      {isOpenInfoBox && <ProfileInfoBox />}
+      {isOpenInfoBox && (
+        <div ref={openedRef}>
+          <ProfileInfoBox />
+        </div>
+      )}
     </div>
   )
 }
