ad775864d1913f5adce7de92cbf7f339c0912af8
Hyejun An <jagaldol.dev@gmail.com>
2025-02-01 02:59:26 +0900

    feat: 페이지 이동 시 header UI 변하여 뒤로가기 버튼 생성

--- chrome-extension/src/containers/main/header/Header.tsx
+++ chrome-extension/src/containers/main/header/Header.tsx
@@ -1,9 +1,14 @@
 import { TbBrandGmail } from "react-icons/tb"
 import { useEffect, useState } from "react"
+import { useRecoilState } from "recoil"
+import { FaAngleLeft } from "react-icons/fa6"
 import Profile from "@/containers/main/header/profile/Profile"
+import viewState from "@/states/viewState"
 
 export default function Header() {
   const [isScrolled, setIsScrolled] = useState(false)
+
+  const [view, setView] = useRecoilState(viewState)
 
   useEffect(() => {
     const handleScroll = () => {
@@ -25,9 +30,15 @@ export default function Header() {
         }
       >
         <span className="text-4xl w-16">
-          <TbBrandGmail />
+          {view.type === "home" ? (
+            <TbBrandGmail />
+          ) : (
+            <button type="button" onClick={() => setView({ type: "home" })}>
+              <FaAngleLeft />
+            </button>
+          )}
         </span>
-        <h1 className="text-xl">Daily Report</h1>
+        <h1 className="text-xl">{view.type === "home" && "Daily Report"}</h1>
         <div className="w-16 flex justify-end">
           <Profile />
         </div>
