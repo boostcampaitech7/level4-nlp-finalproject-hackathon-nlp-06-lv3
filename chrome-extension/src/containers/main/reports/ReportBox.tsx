dec3f4a492b18db9c393b24e7166e8f32d04a308
Hyejun An <jagaldol.dev@gmail.com>
2025-02-01 03:32:00 +0900

    feat: 리포트 카드에도 마크다운 적용

--- chrome-extension/src/containers/main/reports/ReportBox.tsx
+++ chrome-extension/src/containers/main/reports/ReportBox.tsx
@@ -1,4 +1,6 @@
 import { useSetRecoilState } from "recoil"
+import remarkGfm from "remark-gfm"
+import Markdown from "react-markdown"
 import viewState from "@/states/viewState"
 import ReportTitle from "@/containers/main/reports/ReportTitle"
 
@@ -7,11 +9,19 @@ export default function ReportBox({ report }: { report: any }) {
   return (
     <button
       type="button"
-      className="flex flex-col w-full bg-white rounded-lg p-6 gap-2 min-h-[170px] border border-border-gray shadow hover:brightness-[.97] hover:drop-shadow-lg transition-all"
+      className="flex flex-col w-full bg-white rounded-lg px-6 pt-6 gap-2 h-[170px] border border-border-gray shadow hover:brightness-[.97] hover:drop-shadow-lg transition-all overflow-hidden group"
       onClick={() => setView({ type: "report", data: report })}
     >
       <ReportTitle dateString={report.date} />
-      <p className="text-text-gray">{report.content}</p>
+      <Markdown
+        className="text-left text-text-gray line-clamp-3 text-ellipsis markdown-container"
+        remarkPlugins={[remarkGfm]}
+      >
+        {report.content}
+      </Markdown>
+      <div className="flex flex-1 justify-end items-end w-full pt-1 pb-3">
+        <span className="text-xs text-text-gray group-hover:text-black">자세히 보기</span>
+      </div>
     </button>
   )
 }
