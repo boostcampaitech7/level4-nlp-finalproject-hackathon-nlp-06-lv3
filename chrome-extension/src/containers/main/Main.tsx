e1ff66829830c72fee262d87535b09131c3803f8
Hyejun An <jagaldol.dev@gmail.com>
2025-02-01 02:17:34 +0900

    feat: 야매 router 추가
    url로 routing이 안되니까 현재 view가 무엇인지 저장할 viewState 구현

--- chrome-extension/src/containers/main/Main.tsx
+++ chrome-extension/src/containers/main/Main.tsx
@@ -1,12 +1,17 @@
+import { useRecoilValue } from "recoil"
 import Header from "@/containers/main/header/Header"
 import ReportsContainers from "@/containers/main/reports/ReportsContainers"
+import viewState from "@/states/viewState"
 
 export default function Main() {
+  const view = useRecoilValue(viewState)
+
   return (
     <div className="flex flex-col justify-center w-full">
       <Header />
       <div className="p-7">
-        <ReportsContainers />
+        {view.type === "home" && <ReportsContainers />}
+        {view.type === "report" && <div>상세 페이지</div>}
       </div>
     </div>
   )
