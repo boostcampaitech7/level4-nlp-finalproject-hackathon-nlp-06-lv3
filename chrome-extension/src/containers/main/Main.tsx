b4c32964fa6960766311e44836822e0150cdb757
Hyejun An <jagaldol.dev@gmail.com>
2025-02-01 03:11:32 +0900

    feat: 스크롤 내려가있을때 페이지 이동 시 스크롤 남아있는 문제 해결
    단 뒤로 가기 시에도 항상 맨위로 감
    이 부분은 router 에 scroll 상태 및 이동 경로를 저장해야하는데 이럴 경우 router 라이브러리를 사용하는게 나을것이므로 방치

--- chrome-extension/src/containers/main/Main.tsx
+++ chrome-extension/src/containers/main/Main.tsx
@@ -1,4 +1,5 @@
 import { useRecoilValue } from "recoil"
+import { useEffect } from "react"
 import Header from "@/containers/main/header/Header"
 import ReportsContainers from "@/containers/main/reports/ReportsContainers"
 import viewState from "@/states/viewState"
@@ -6,6 +7,10 @@ import ReportPage from "@/containers/main/reports/ReportPage"
 
 export default function Main() {
   const view = useRecoilValue(viewState)
+
+  useEffect(() => {
+    window.scrollTo({ top: 0 })
+  }, [view])
 
   return (
     <div className="flex flex-col justify-center w-full">
