9ec834d73f23ec321175f40ff73c81482f37e758
Hyejun An <jagaldol.dev@gmail.com>
2025-02-01 02:33:42 +0900

    feat: 페이지 이동 및 mouse hovering 상호작용 추가

--- chrome-extension/src/containers/main/reports/ReportBox.tsx
+++ chrome-extension/src/containers/main/reports/ReportBox.tsx
@@ -1,4 +1,6 @@
 import moment from "moment"
+import { useSetRecoilState } from "recoil"
+import viewState from "@/states/viewState"
 
 function ReportTitle({ dateString }: { dateString: string }) {
   const date = moment(dateString)
@@ -15,10 +17,15 @@ function ReportTitle({ dateString }: { dateString: string }) {
 }
 
 export default function ReportBox({ report }: { report: any }) {
+  const setView = useSetRecoilState(viewState)
   return (
-    <div className="flex flex-col w-full bg-white rounded-lg p-6 gap-2 min-h-[170px] border border-border-gray">
+    <button
+      type="button"
+      className="flex flex-col w-full bg-white rounded-lg p-6 gap-2 min-h-[170px] border border-border-gray shadow hover:brightness-[.97] hover:drop-shadow-lg transition-all"
+      onClick={() => setView({ type: "report", data: report })}
+    >
       <ReportTitle dateString={report.date} />
       <p className="text-text-gray">{report.content}</p>
-    </div>
+    </button>
   )
 }
