5c05c30c976895fc84489e2e8670bb81704faec7
Hyejun An <jagaldol.dev@gmail.com>
2025-02-05 07:58:16 +0900

    feat: API Key 변경 기능 완성

--- chrome-extension/src/containers/main/header/profile/UpdateAPIKeyForm.tsx
+++ chrome-extension/src/containers/main/header/profile/UpdateAPIKeyForm.tsx
@@ -1,12 +1,36 @@
+import { useState } from "react"
+import { useMutation, useQueryClient } from "@tanstack/react-query"
 import useOpenLink from "@/hooks/useOpenLink"
+import useUserInfoQuery from "@/hooks/useUserInfoQuery"
+import axiosInstance from "@/utils/axiosInstance"
+import useToast from "@/hooks/useToast"
+import useErrorResponseHandler from "@/hooks/useErrorResponseHandler"
 
 export default function UpdateAPIKeyForm({ onSubmit }: { onSubmit: () => void }) {
+  const { addSuccessToast } = useToast()
   const openLink = useOpenLink()
+
+  const { userInfo } = useUserInfoQuery()
+  const queryClient = useQueryClient()
+  const errorHandler = useErrorResponseHandler()
+
+  const [upstageApiKey, setUpstageApiKey] = useState<string>(userInfo.upstage_api_key)
+
+  const { mutate } = useMutation({
+    mutationFn: (data: any) => axiosInstance.put("/auth/profile", data),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["/auth/google/profile"] }).then()
+      addSuccessToast("API Key가 저장되었습니다.")
+    },
+    onError: (err) => errorHandler(err),
+  })
+
   return (
     <form
       className="flex flex-col gap-4 pt-[98px]"
       onSubmit={(e) => {
         e.preventDefault()
+        mutate({ upstage_api_key: upstageApiKey })
         onSubmit()
       }}
     >
@@ -25,6 +49,12 @@ export default function UpdateAPIKeyForm({ onSubmit }: { onSubmit: () => void })
         <input
           className="w-full h-10 p-2 rounded-lg border border-border-gray text-text-gray focus:bg-gray-50"
           placeholder="Key 입력"
+          value={upstageApiKey}
+          onChange={(e) => setUpstageApiKey(e.target.value)}
+          onBlur={() => {
+            const trimKey = upstageApiKey.replaceAll(" ", "")
+            setUpstageApiKey(trimKey === "" ? userInfo.upstage_api_key : trimKey)
+          }}
         />
       </div>
       <button type="submit" className="w-full h-10 rounded-lg bg-main-theme text-white">
