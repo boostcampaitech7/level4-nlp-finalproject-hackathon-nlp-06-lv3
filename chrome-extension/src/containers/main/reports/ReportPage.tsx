f4495690dff6b3cf8a0a1250a7d21c1670781120
jagaldol <yoy06056@pusan.ac.kr>
2025-02-06 14:55:15 +0900

    feat: report json 파싱

--- chrome-extension/src/containers/main/reports/ReportPage.tsx
+++ chrome-extension/src/containers/main/reports/ReportPage.tsx
@@ -1,18 +1,36 @@
 import { useRecoilValue } from "recoil"
-import Markdown from "react-markdown"
-import remarkGfm from "remark-gfm"
 import ReportTitle from "@/containers/main/reports/ReportTitle"
 import viewState from "@/states/viewState"
 
 export default function ReportPage() {
   const view = useRecoilValue(viewState)
   const report = view.data
+  const jsonReport = JSON.parse(report.content)
+
   return (
     <div className="flex flex-col w-full bg-white rounded-lg p-6 gap-2 min-h-[170px] border border-border-gray drop-shadow-small">
       <ReportTitle dateString={report.date} />
-      <Markdown className="text-text-gray markdown-container -mt-3 break-words" remarkPlugins={[remarkGfm]}>
-        {report.content}
-      </Markdown>
+
+      {jsonReport.map((category: any) => (
+        <div key={category.title}>
+          <h3>{category.title}</h3>
+          {category.task_objects.map((task: any) => (
+            <div key={task.title}>
+              <h4>{task.title}</h4>
+              {task.items.map((item: any) => (
+                <div key={item.description}>
+                  <p>{item.description}</p>
+                  <ul>
+                    {item.links.map((link: any) => (
+                      <li key={link}>{link}</li>
+                    ))}
+                  </ul>
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
+      ))}
     </div>
   )
 }
