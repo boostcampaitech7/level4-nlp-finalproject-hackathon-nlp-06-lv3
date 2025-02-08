429590123e1e03faaa71fcb8c706aea786ab2a3c
jagaldol <yoy06056@pusan.ac.kr>
2025-02-08 14:11:22 +0900

    chore: 빠른 디버깅을 위한 세팅 변경

--- batch_main.py
+++ batch_main.py
@@ -8,8 +8,8 @@ def main():
 
     # access token, refresh token 가져와서 service 객체 선언하기
     for user in users:
-        try:
-            # if user["id"] == 9:
+        # try:
+        if user["id"] == 9:
             service = authenticate_gmail(user)
             # GmailService 인스턴스 생성
             gmail_service = GmailService(service)
@@ -21,8 +21,8 @@ def main():
 
             insert_report(user["id"], report, json_checklist)
 
-        except Exception as e:
-            print(e)
+        # except Exception as e:
+        #     print(e)
 
 
 if __name__ == "__main__":
