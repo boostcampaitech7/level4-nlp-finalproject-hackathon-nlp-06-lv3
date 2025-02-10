8bca096460245e6fe7cab354ab4d030dc61833a7
Gamchan Kang <gsgh3016@gmail.com>
2025-02-10 18:17:05 +0900

    feat: 파이프라인에서 분류 평가용 데이터 생성 자동화

--- pipelines/classify_single_mail.py
+++ pipelines/classify_single_mail.py
@@ -1,4 +1,7 @@
 import warnings
+from collections import Counter
+
+import pandas as pd
 
 from agents.classification.classification_agent import ClassificationAgent
 from agents.classification.classification_type import ClassificationType
@@ -14,14 +17,32 @@ def classify_single_mail(mail_dict: dict[str, Mail], summary_dict: dict[str, str
 
     classification_agent = ClassificationAgent("solar-pro", temperature, seed)
 
-    category_dict = {}
-    action_dict = {}
+    categories_dict = {}
+    actions_dict = {}
+
+    iteration = Config.config["classification"]["inference"]
+
+    df = pd.DataFrame(columns=["categories", "actions"], index=list(mail_dict.keys()))
+    df.index.name = "id"
 
     for mail_id, mail in mail_dict.items():
-        category = classification_agent.process(summary_dict[mail_id], ClassificationType.CATEGORY)
-        action = classification_agent.process(summary_dict[mail_id], ClassificationType.ACTION)
+        categories = [
+            classification_agent.process(summary_dict[mail_id], ClassificationType.CATEGORY) for i in range(iteration)
+        ]
+        actions = [
+            classification_agent.process(summary_dict[mail_id], ClassificationType.ACTION) for i in range(iteration)
+        ]
 
-        category_dict[mail_id] = category
-        action_dict[mail_id] = action
+        categories_dict[mail_id] = categories
+        actions_dict[mail_id] = actions
+
+        df.loc[mail_id, ["categories", "actions"]] = [categories, actions]
+
+    df.to_csv("evaluation/data/generated_category.csv")
+
+    category_dict = {
+        mail_id: Counter(categories).most_common(1)[0][0] for mail_id, categories in categories_dict.items()
+    }
+    action_dict = {mail_id: Counter(actions).most_common(1)[0][0] for mail_id, actions in actions_dict.items()}
 
     return category_dict, action_dict
