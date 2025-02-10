ffe33748966863ca1e6cbf1c534d680fdb8e6b0d
jagaldol <yoy06056@pusan.ac.kr>
2025-02-10 18:53:52 +0900

    refactor: print 문 추출 및 개선

--- agents/reflexion/reflexion.py
+++ agents/reflexion/reflexion.py
@@ -20,8 +20,10 @@ class ReflexionFramework:
         Returns:
             모든 aspect 점수의 평균값이 제일 높은 text가 반환됩니다.
         """
-        scores = []
         outputs = []
+        eval_results = []
+        final_output = ""
+        max_score = 0
 
         for i in range(self.max_iteration):
             # 출력문 재생성
@@ -30,42 +32,46 @@ class ReflexionFramework:
 
             # 평가하기
             eval_result: dict = self.evaluator.get_geval_scores(origin_mail, output_text)
-            scores.append(round(sum(eval_result.values()) / len(eval_result), 1))
+            eval_results.append(eval_result)
+            score = round(sum(eval_result.values()) / len(eval_result), 1)
 
-            eval_average = round(sum(eval_result.values()) / len(eval_result), 1)
-            self._print_reflection_process(eval_result, output_text, eval_average)
-
-            if eval_average >= self.threshold:
-                break
+            if max_score < score:
+                max_score = score
+                final_output = output_text
+                if round(sum(eval_result.values()) / len(eval_result), 1) >= self.threshold:
+                    break
 
             # 점수 도달 실패 시 이유 성찰
             self.self_reflection.generate_reflection(
                 origin_mail, output_text, self._create_eval_result_str(eval_result)
             )
 
-        self._print_result(scores, outputs)
-
-        _, final_output = max(zip(scores, outputs), key=lambda x: x[0])
+        self._print_result(eval_results, outputs)
 
         return final_output
 
     def _create_eval_result_str(self, eval_result: dict):
         return "\n".join([f"항목: {aspect} 점수: {score}" for aspect, score in eval_result.items()])
 
-    def _print_reflection_process(self, eval_result: dict, output_text: str, score: float):
-        print(
-            f"{'=' * 25}\n"
-            f"Reflection 메모리:\n{self.self_reflection.get_reflection_memory_str()}\n\n"
-            f"{'-' * 25}\n"
-            f"생성된 텍스트:\n{output_text}"
-            f"{'-' * 25}\n"
-            f"{self._create_eval_result_str(eval_result)}, 평균 {score}점\n"
-        )
+    def _print_result(self, eval_results: list[dict], outputs: list[str]):
+        max_score = 0
+        final_output = ""
+        max_index = 0
 
-    def _print_result(self, scores, outputs):
-        for i, score in enumerate(scores):
-            print(f"{i+1}회차 평균 {score}점")
-        print(f"{'='*25}\n")
-        best_index = max(enumerate(scores), key=lambda x: x[1])[0]
-        print(f"최종 출력문 ({best_index + 1}회차, 평균: {scores[best_index]}점)")
-        print(outputs[best_index])
+        for i, (eval_result, output) in enumerate(zip(eval_results, outputs)):
+            score = round(sum(eval_result.values()) / len(eval_result), 1)
+            print(
+                f"{'=' * 25}\n"
+                f"{i+1}회차 평균 {score}점\n"
+                f"{self._create_eval_result_str(eval_result)}\n"
+                f"Reflection 메모리:\n{self.self_reflection.get_reflection_memory_str()}\n"
+                f"{'-' * 25}\n"
+                f"생성된 텍스트:\n{output}\n"
+                f"{'-' * 25}\n"
+            )
+            if max_score < score:
+                max_score = score
+                final_output = output
+                max_index = i
+
+        print(f"{'=' * 25}\n최종 출력:{max_index+1}회차 평균 {max_score}점\n{final_output}\n{'=' * 25}\n")
